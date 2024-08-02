82bae4e24936667ce756f11eb5fa7fefebfb618c
Ali Ganji <aliganji142879@gmail.com>
2024-08-02 15:51:52 +0330

    extending user

--- account/admin.py
+++ account/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+from .models import Relation, Profile
 
-from .models import Relation
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+class ExtendedUserAdmin(UserAdmin):
+    inlines = (ProfileInline,)
+
+
+admin.site.unregister(get_user_model())
+admin.site.register(get_user_model(), ExtendedUserAdmin)
 
 
 @admin.register(Relation)
-class PostAdmin(admin.ModelAdmin):
+class RelationAdmin(admin.ModelAdmin):
+    list_display = ['from_user', 'to_user', 'created']
     search_fields = ['from_user', 'to_user']
-    list_filter = ['from_user', 'to_user', 'created']
+    list_filter = ['created']
     raw_id_fields = ['from_user', 'to_user']
--- account/models.py
+++ account/models.py
@@ -9,3 +9,9 @@ class Relation(models.Model):
 
     def __str__(self):
         return f"{self.from_user} following {self.to_user}"
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    age = models.PositiveSmallIntegerField(default=0)
+    bio = models.TextField(null=True, blank=True)
